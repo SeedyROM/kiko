aa689532914963d95a1e9ab71db2a70aa0e3156c
Zack Kollar <rallokkcaz@gmail.com>
2025-05-26 01:21:16 -0400

    :hammer: Remove stupid add lib export, bad

--- crates/kiko/src/lib.rs
+++ crates/kiko/src/lib.rs
@@ -5,18 +5,3 @@ pub mod log;
 
 pub use serde;
 pub use serde_json;
-
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
